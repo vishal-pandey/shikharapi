f6470950f0e12296d86da39e71738231aca97bf5
vishal-pandey <pandeyvishal25@yahoo.com>
2018-05-03 07:31:28 +0530

    script upload

--- work/uploadresume.php
+++ work/uploadresume.php
@@ -6,7 +6,7 @@
 	$email = $_POST['email'];
 	// $resume = $_POST['resume'];
 
-	$sql = "insert into resume values ('{fname}' , '{lname}', '{email}')";
+	$sql = "insert into resume values ('{$fname}' , '{$lname}', '{$email}')";
 
 	if($conn->query($sql)){
 		echo "success";
