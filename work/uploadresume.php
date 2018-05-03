3ce163a6d8a0945796b70034580b9d9dae6083a6
vishal-pandey <pandeyvishal25@yahoo.com>
2018-05-03 07:43:13 +0530

    script upload

--- work/uploadresume.php
+++ work/uploadresume.php
@@ -5,11 +5,17 @@
 	$lname = $_POST['lname'];
 	$email = $_POST['email'];
 	// $resume = $_POST['resume'];
+	$resume = $_FILES['resume']['name'];
 
 	$sql = "insert into resume values ('{$fname}' , '{$lname}', '{$email}')";
 
 	if($conn->query($sql)){
-		echo "success";
+		// echo "success";
+		$dir = "../resume/";
+		$target_file = $dir.$resume;
+		if(move_uploaded_file($_FILES["photo"]["tmp_name"], $target_file)){
+			echo "success";
+		}
 	}
 
 ?>
