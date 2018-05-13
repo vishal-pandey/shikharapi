81dbde8412fa0936856f13f46120b1d9327a3d97
vishal-pandey <pandeyvishal25@yahoo.com>
2018-05-13 20:12:15 +0530

    script upload

--- query/query.php
+++ query/query.php
@@ -4,4 +4,9 @@
 	$mobile = $_POST['mobile'];
 	$email = $_POST['email'];
 	$query = $_POST['query'];
+
+	$sql = "insert into contacts values ('{$name}','{$mobile}','{$email}','{$query}')";
+	if($conn->query($sql)){
+		echo "success";
+	}
 ?>
