9af76bf15071e541637abb9937826a86ee1e1cb4
vishal-pandey <pandeyvishal25@yahoo.com>
2018-05-13 20:10:10 +0530

    script upload

--- db/build.sql
+++ db/build.sql
@@ -1,1 +1,2 @@
 create table resume (fname text, lname text, email text, resumelink text);
+create table contacts(name text, mobile text, email text, query text);
---
+++ query/query.php
@@ -0,0 +1,7 @@
+<?php
+	include "../db/connect.php";
+	$name = $_POST['name'];
+	$mobile = $_POST['mobile'];
+	$email = $_POST['email'];
+	$query = $_POST['query'];
+?>
